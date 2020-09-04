bc2212080cb2d4ebcd56fe91ec41a21a4ac320cb
adetiamarhadi <adetia.marhadi@gmail.com>
2020-09-05 05:24:10 +0700

    Assignment 3 - Practicing Directives

---
+++ src/app/assignment-three/assignment-three.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-assignment-three',
+  templateUrl: './assignment-three.component.html',
+  styleUrls: ['./assignment-three.component.css']
+})
+export class AssignmentThreeComponent implements OnInit {
+
+  showSecret = false;
+
+  log = [];
+
+  onToggleDetails() {
+    this.showSecret = !this.showSecret;
+    this.log.push(this.log.length + 1);
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
